247517ab44b17b14fd468f0bbe3adb69b669fc4b
Alexandre Coelho <116454874+coelhoalexandre@users.noreply.github.com>
2024-03-02 17:23:21 -0300

    Update metodoFilter.js

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,6 +5,8 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
+    
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
 
     let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
 
